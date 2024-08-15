2ba42e91cf6ebd032240630b83282a67f314c847
Vindhya Bhagavan <vindhya.bhagavan@gmail.com>
2024-08-15 14:19:09 +0200

    updated unit test and package.json files

--- __tests__/unit/unit.js
+++ __tests__/unit/unit.js
@@ -7,12 +7,28 @@ describe("numberCruncher tests", function () {
     const result = numberCruncher(calcObject);
     expect(result).toBe(9);
   });
-
-  // Regression test
-  it("should return 9 when adding 2 and 7", function () {
-    const calcObject = { numOne: Infinity, numTwo: 1, operator: "add" };
-    const result = numberCruncher(calcObject);
-    expect(result).toBe(Infinity);
-  });
 });
 
+describe("numberCruncher tests", function () {
+    it("should return 6 when subtracting 9 from 3", function () {
+        const calcObject = { numOne: 9, numTwo: 3, operator: "subtract"};
+        const result = numberCruncher(calcObject);
+        expect(result).toBe(6);
+    });
+})
+
+describe("numberCruncher tests", function () {
+    it("should return 24 when multiplying 6 and 4", function () {
+        const calcObject = { numOne: 6, numTwo: 4, operator: "multiply"};
+        const result = numberCruncher(calcObject);
+        expect(result).toBe(24);
+    });
+})
+
+describe("numberCruncher tests", function () {
+    it("should return 1 when multiplying 10 and 10", function () {
+        const calcObject = { numOne: 10, numTwo: 10, operator: "divide"};
+        const result = numberCruncher(calcObject);
+        expect(result).toBe(1);
+    });
+})
