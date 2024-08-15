d9a1188f8199869553ad280923fd013595826bdd
Vindhya Bhagavan <vindhya.bhagavan@gmail.com>
2024-08-15 15:16:29 +0200

    updated test folders

---
+++ __tests__/integration/integration.js
@@ -0,0 +1,34 @@
+import numberCruncher from "../../src/modules/calculation/numberCruncher";
+
+describe("numberCruncher tests", function () {
+
+  it("should return 9 when adding 2 and 7", function () {
+    const calcObject = { numOne: 2, numTwo: 7, operator: "add" };
+    const result = numberCruncher(calcObject);
+    expect(result).toBe(9);
+  });
+});
+
+describe("numberCruncher tests", function () {
+    it("should return 6 when subtracting 9 from 3", function () {
+        const calcObject = { numOne: 9, numTwo: 3, operator: "subtract"};
+        const result = numberCruncher(calcObject);
+        expect(result).toBe(6);
+    });
+})
+
+describe("numberCruncher tests", function () {
+    it("should return 24 when multiplying 6 and 4", function () {
+        const calcObject = { numOne: 6, numTwo: 4, operator: "multiply"};
+        const result = numberCruncher(calcObject);
+        expect(result).toBe(24);
+    });
+})
+
+describe("numberCruncher tests", function () {
+    it("should return 1 when multiplying 10 and 10", function () {
+        const calcObject = { numOne: 10, numTwo: 10, operator: "divide"};
+        const result = numberCruncher(calcObject);
+        expect(result).toBe(1);
+    });
+})
--- __tests__/unit/unit.js
+++ __tests__/unit/unit.js
@@ -1,34 +1,48 @@
 import numberCruncher from "../../src/modules/calculation/numberCruncher";
+import percentage from "../../src/modules/calculation/util/percentage";
+import gcd from "../../src/modules/calculation/util/gcd";
+import exp from "../../src/modules/calculation/util/exp";
 
-describe("numberCruncher tests", function () {
-  // Functional test
-  it("should return 9 when adding 2 and 7", function () {
-    const calcObject = { numOne: 2, numTwo: 7, operator: "add" };
-    const result = numberCruncher(calcObject);
-    expect(result).toBe(9);
-  });
+describe("gcd tests", function() {
+    it("should result in 1 when given 4 and 5", function() {
+        const calcObject = { numOne: 4, numTwo: 5, operator: "gcd" };
+        const result = numberCruncher(calcObject);
+        expect(gcd(4, 5)).toBe(1);
+    });
+
+})
+
+describe("exponent tests", function() {
+    it("should result in 125 when given 5 and 3", function() {
+        const calcObject = { numOne: 5, numTwo: 3, operator: "exp" };
+        const result = numberCruncher(calcObject);
+        expect(exp(5, 3)).toBe(125);
+    });
+
+})
+
+describe("percentage tests", function() {
+    it("should result in 50 when given 3 and 6", function() {
+        const calcObject = { numOne: 3, numTwo: 6, operator: "percentage" };
+        const result = numberCruncher(calcObject);
+        expect(percentage(3, 6)).toBe(50);
+    });
+    
+    it("should result in -1 when given -1 and 100", function() {
+        const calcObject = { numOne: -1, numTwo: 100, operator: "percentage" };
+        const result = numberCruncher(calcObject);
+        expect(result).toBe(-1);
+    });
+
+    it("should result in Infinity when given 1 and 0", function() {
+        const calcObject = { numOne: 1, numTwo: 0, operator: "percentage" };
+        const result = numberCruncher(calcObject);
+        expect(percentage(1, 0)).toBe(Infinity);
+    });
+
+    it("should result in 1000 when given 1000 and 100", function() {
+        const calcObject = { numOne: 1000, numTwo: 100, operator: "percentage" };
+        const result = numberCruncher(calcObject);
+        expect(percentage(1000, 100)).toBe(1000);
+    });
 });
-
-describe("numberCruncher tests", function () {
-    it("should return 6 when subtracting 9 from 3", function () {
-        const calcObject = { numOne: 9, numTwo: 3, operator: "subtract"};
-        const result = numberCruncher(calcObject);
-        expect(result).toBe(6);
-    });
-})
-
-describe("numberCruncher tests", function () {
-    it("should return 24 when multiplying 6 and 4", function () {
-        const calcObject = { numOne: 6, numTwo: 4, operator: "multiply"};
-        const result = numberCruncher(calcObject);
-        expect(result).toBe(24);
-    });
-})
-
-describe("numberCruncher tests", function () {
-    it("should return 1 when multiplying 10 and 10", function () {
-        const calcObject = { numOne: 10, numTwo: 10, operator: "divide"};
-        const result = numberCruncher(calcObject);
-        expect(result).toBe(1);
-    });
-})
